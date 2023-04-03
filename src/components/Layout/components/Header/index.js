efc68ad890c2cd73bd3e6c3945fd2fb406f2eba9
Duy Tran <duy88tran@mail.com>
2023-04-03 22:21:50 +0700

    Header-UI-1

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,11 +1,29 @@
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import classNames from 'classnames/bind';
+import images from '~/assets/images';
 import styles from './Header.module.scss';
 
 const cs = classNames.bind(styles);
+
 function Header() {
   return (
     <header className={cs('wrapper')}>
-      <div className={cs('inner')}></div>
+      <div className={cs('inner')}>
+        <img src={images.logo} alt="Tiktok" />
+        <div className={cs('search')}>
+          <input placeholder="search accounts and videos" />
+          <button className={cs('clear')}>
+            <FontAwesomeIcon icon={faCircleXmark} />
+          </button>
+          <FontAwesomeIcon className={cs('loading')} icon={faSpinner} />
+          {/* loading */}
+          <button className={cs('search-btn')}>
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </button>
+        </div>
+        <div className={cs('actions')}></div>
+      </div>
     </header>
   );
 }
